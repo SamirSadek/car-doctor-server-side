f9eee92813556972e88fe3efd50f65981ef7395f
Samir Sadek <samirsadek2009@gmail.com>
2023-10-26 09:14:03 +0600

    insert checkout page data into MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -29,9 +29,36 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const servicesCollection = client.db("carDoctor").collection("services")
+    const checkoutCollection = client.db('carDoctor').collection('checkouts')
+
+
+    app.get('/services',async(req, res)=>{
+        const cursor = servicesCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            projection:{title:1, price:1, service_id: 1}
+        }
+        const result = await servicesCollection.findOne(query,options)
+        res.send(result)
+    })
+
+    // checkouts
+    app.post('/checkouts', async(req,res)=>{
+        const checking = req.body;
+        const result = await checkoutCollection.insertOne(checking)
+        res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
